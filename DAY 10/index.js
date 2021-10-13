2ad4b51ceb478b582962b2be212b388012e0241b
Deepak Singh <deepaksingh66013@gmail.com>
2021-10-13 17:50:56 +0530

    day 10

--- DAY 10/index.js
+++ DAY 10/index.js
@@ -1,24 +1,25 @@
-const express  = require('express');
-const path  = require('path');
+const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set("views", path.join(__dirname, "views")); // first define the path of the views folder
 app.set('view engine', 'hbs'); // then use it 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname,'public','index.html'));
     // res.render('index', {title: 'handlebars'});
     res
-    .status(201)
-    .cookie("token", "token", {
-        expire: new Date(Date.now() + 4 *360000)
-    })
-    .cookie("test", "Test")
-    .redirect(301,'/admin')
+        .status(201)
+        .cookie("token", "token", {
+            expire: new Date(Date.now() + 4 * 360000)
+        })
+        .cookie("test", "Test")
+
+        .redirect(301, '/admin')
 })
 
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log(`Listening to http://localhost:5000`);
 })
